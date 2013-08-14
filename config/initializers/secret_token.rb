b1fba76f5719cd3e17d13a12c56b0e0769dd8c23
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-14 12:15:49 -0700

    add rails 4 skip  testing service

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseTester::Application.config.secret_key_base = '0ad35dc53080f672f9b348ea658698b9fbb313d7407c24a03ba1de8526d32830a7cd110630315de531800d8b263fd16aff5d9dd6285c091d900c86d387609be2'
